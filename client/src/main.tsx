c0557a927d1a66af4df812f0012a60505e120884
mithiiiii1811 <taekookbestlove@gmail.com>
2025-11-02 20:40:40 +0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,5 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './ui/App'
 import './index.css'
+import { io } from "socket.io-client";
+
+const socket = io("https://bargaining-game-batna-2.onrender.com", {
+  transports: ["websocket"],
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
