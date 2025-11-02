25f7f7d4f9f917fe6b377a6d3eb047c6a2025d2d
mithiiiii1811 <taekookbestlove@gmail.com>
2025-11-02 20:52:54 +0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './ui/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./ui/App";
+import "./index.css";
 import { io } from "socket.io-client";
 
-const socket = io("https://bargaining-game-batna-2.onrender.com", {
-  transports: ["websocket"],
-});
+const socket = io(
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5174"
+    : "https://bargaining-game-batna-2.onrender.com",
+  {
+    transports: ["websocket"],
+  }
+);
 
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+export { socket };
