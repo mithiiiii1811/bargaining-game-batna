e0c9fee4dac2b3bfa8fb532c6892a804dc9edf1b
mithiiiii1811 <taekookbestlove@gmail.com>
2025-11-02 22:22:52 +0500

    Update App.tsx

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -16,7 +16,7 @@ const SERVER_URL =
   import.meta.env.VITE_SERVER_URL ||
   (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:5174"
-    : `${window.location.protocol}//${window.location.host}`);
+     : "https://bargaining-game-batna-2.onrender.com");
 
 
 function MidpointCard() {
